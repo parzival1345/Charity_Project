95f6f68980a071ede9917d721ec5e3aabf6a836d
moein <mmoein.motamed@gmail.com>
2024-03-23 03:19:09 +0330

    making pass to hash i forgot it

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthenticationController extends Controller
 {
@@ -15,7 +16,7 @@ class AuthenticationController extends Controller
             'last_name' => $request->l_name,
             'phone_number' => $request->p_number,
             'email' => $request->email,
-            'password' => $request->pass
+            'password' => Hash::make($request->pass)
         ]);
         return response()->json([
             'status' => true,

1a2218417a19ac4f1455cdc19fd091ae9ed261fe
moein <mmoein.motamed@gmail.com>
2024-03-23 03:16:44 +0330

    add RegisterRequest instead of Request

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -2,18 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use App\Events\Register;
+use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthenticationController extends Controller
 {
-    public function register(Request $request)
+    public function register(RegisterRequest $request)
     {
         User::create([
             'first_name' => $request->f_name,
             'last_name' => $request->l_name,
             'phone_number' => $request->p_number,
+            'email' => $request->email,
             'password' => $request->pass
         ]);
         return response()->json([

0081e42050e9067ecccf58710ad7cefeef6af9f2
moein <mmoein.motamed@gmail.com>
2024-03-23 02:41:48 +0330

    add auth controller

---
+++ app/Http/Controllers/AuthenticationController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Events\Register;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthenticationController extends Controller
+{
+    public function register(Request $request)
+    {
+
+    }
+}

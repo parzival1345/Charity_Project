0b553243a7ce24af547c619a907e40892eb4af02
moein <mmoein.motamed@gmail.com>
2024-03-23 02:54:40 +0330

    test register code

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -10,6 +10,15 @@ class AuthenticationController extends Controller
 {
     public function register(Request $request)
     {
-
+        User::create([
+            'first_name' => $request->f_name,
+            'last_name' => $request->l_name,
+            'phone_number' => $request->p_number,
+            'password' => $request->pass
+        ]);
+        return response()->json([
+            'status' => true,
+            'message' => 'ok'
+        ]);
     }
 }

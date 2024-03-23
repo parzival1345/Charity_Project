2cc0bbe04e0ea7d3bb897b171975231fe5945a6c
moein <mmoein.motamed@gmail.com>
2024-03-23 04:09:25 +0330

    create sms function for sending sms code for confirmation

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\Register;
+use App\Events\SmsEvent;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use function PHPUnit\Framework\callback;
 
 class AuthenticationController extends Controller
 {
@@ -18,9 +21,23 @@ class AuthenticationController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->pass)
         ]);
+        $sms = $this->sms();
         return response()->json([
             'status' => true,
-            'message' => 'ok'
+            'message' => 'register process working well'
         ]);
+    }
+
+    public function sms(RegisterRequest $request)
+    {
+        $user = User::create([
+            'first_name' => $request->f_name,
+            'last_name' => $request->l_name,
+            'phone_number' => $request->p_number,
+            'email' => $request->email,
+            'password' => Hash::make($request->pass),
+            'status' => 'deny'
+        ]);
+        event(new SmsEvent($user));
     }
 }

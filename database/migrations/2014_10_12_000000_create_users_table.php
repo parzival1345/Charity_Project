b25f6f25031a443bd8597a8a4f2637911beb26f5
moein <mmoein.motamed@gmail.com>
2024-03-23 22:03:07 +0330

    add a defualt for status -> deny

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->enum('sms_confirmation' , ['yes','no']);
-            $table->enum('status' , ['deny','accept']);
+            $table->enum('status' , ['deny','accept'])->default('deny');
             $table->timestamps();
         });
     }

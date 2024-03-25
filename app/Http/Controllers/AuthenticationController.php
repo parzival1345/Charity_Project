50115173d27523752a50913db1c08477e25c2cc8
moein <mmoein.motamed@gmail.com>
2024-03-25 15:27:24 +0330

    deleting 2 function and change function sms to function register becus that is more complete

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -8,27 +8,14 @@ use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use MongoDB\Driver\Session;
+use function Laravel\Prompts\select;
 use function PHPUnit\Framework\callback;
 
+session_start();
 class AuthenticationController extends Controller
 {
     public function register(RegisterRequest $request)
-    {
-        User::create([
-            'first_name' => $request->f_name,
-            'last_name' => $request->l_name,
-            'phone_number' => $request->p_number,
-            'email' => $request->email,
-            'password' => Hash::make($request->pass),
-            'status' => 'accept'
-        ]);
-        return response()->json([
-            'status' => true,
-            'message' => 'register process working well'
-        ]);
-    }
-
-    public function sms(RegisterRequest $request)
     {
         $user = User::create([
             'first_name' => $request->f_name,
@@ -38,15 +25,10 @@ class AuthenticationController extends Controller
             'password' => Hash::make($request->pass),
             'sms_confirmation' => 'no'
         ]);
-        event(new SmsEvent($user));
+                event(new SmsEvent($user));
         return response()->json([
             'status' => true,
             'message' => 'sms send'
         ]);
-    }
-
-    public function confirmation_code()
-    {
-
     }
 }

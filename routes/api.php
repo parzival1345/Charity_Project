1b8290dad2ad1581af29e4a5b7fa4f037ba70f42
moein <mmoein.motamed@gmail.com>
2024-03-31 13:57:02 +0330

    create route for zarinpal for testing

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthenticationController;
 use App\Http\Controllers\Password_ManagerController;
+use App\Http\Controllers\Zarinpal_Controller;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,6 +26,8 @@ Route::post('/logout/{id}' , [AuthenticationController::class , 'logout']);
 Route::post('/sms_register_check/{id}' , [AuthenticationController::class , 'sms_register_check']);
 //-------password-------//
 Route::post('/send' , [Password_ManagerController::class , 'send_sms']);
+Route::post('/resend_sms' , [Password_ManagerController::class , 'repeat_code']);
 Route::post('/check/{id}' , [Password_ManagerController::class , 'check_sms']);
 Route::post('/change_pass/{id}' , [Password_ManagerController::class , 'reset_password']);
+Route::post('/payment' , [Zarinpal_Controller::class , 'payment']);
 
